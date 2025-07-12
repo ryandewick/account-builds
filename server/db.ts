3ddf1bc9679ccb8027c25f5a34332292dda69863
Ryan Dewick <ryan.dewick@hermes-europe.co.uk>
2025-07-12 18:39:10 +0100

    small adjustment to env file in server dir

--- server/db.ts
+++ server/db.ts
@@ -1,14 +1,28 @@
-import { Pool } from "pg";
-import dotenv from "dotenv";
+// import { Pool } from "pg";
+// import dotenv from "dotenv";
+//
+// dotenv.config();
+//
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   port: Number(process.env.DB_PORT),
+//   database: process.env.DB_DATABASE,
+// });
+//
+// export default pool;
+
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_DATABASE,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false // Required for Render SSL DB connections
+    }
 });
 
 export default pool;
